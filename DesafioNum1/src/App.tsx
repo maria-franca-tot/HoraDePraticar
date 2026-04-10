276325c0a7a321e2239249b9e28de9d1c129bcad
maria-franca-tot <maria.franca.toteduca@email.com>
2026-04-10 07:44:47 -0300

    commit teste

--- DesafioNum1/src/App.tsx
+++ DesafioNum1/src/App.tsx
@@ -1,121 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import "./App.css";
+import imagem from "./img/image.png";
+//import { useState } from "react"; 
 
-function App() {
-  const [count, setCount] = useState(0)
+//import { Button, Grid } from '@mui/material/Grid';
+//import Grid from 
+//use state vai guardar a informacao q muda na tela OU SEJA vai vai guardar a informacao
+//de qual alternativa que eu cliquei dando um numero de indice pra ela onde minha funcao vai 
+// verificar se ta certo e aparecer a msg na tela
+
+export function Exercicio(
+  { title,
+    text,
+    subtitle,
+    italictext, }:
+    {
+      text: string;
+      title: string;
+      subtitle: string;
+      italictext: string;
+      alternativas: string[];
+      alternativaCorreta: number;
+
+      feedback: {
+        correct: {
+          title: string;
+          text: string;
+        };
+        incorrect: {
+          title: string;
+          text: string;
+        };
+      };
+    }
+
+
+) {
+//const [selecionada, setSelecionada] = useState<number | null>(null); 
+  //ou ela pode ser um numero ou nao pode ser nd (null) pq qnd eu clico ela vira um numero
+  //selecionada → valor atual setSelecionada → função que muda o valor
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className="cardQuestao">
+      <div className="blocosDeTexto">
+        <h2>{title}</h2>
+        <p>{text}</p>
+        <h4>{subtitle}</h4>
+        <p><em>{italictext}</em></p>
+      </div>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
+      <div className="conteudo">
+        <div className="EsquerdaImg">
+          <img src={imagem} alt="Homem mexendo no computador" />
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+        <div></div>
+      </div>
+         <button>Enviar</button>
+    </div>
   )
-}
+} 
 
-export default App
+
+
+export default Exercicio;
