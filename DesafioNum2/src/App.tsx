11d8b30d5d06df9ec599c1f30fe8e5def9f37fe2
maria-franca-tot <maria.franca.toteduca@email.com>
2026-04-13 17:05:53 -0300

    inicialização do desafio 2

--- DesafioNum2/src/App.tsx
+++ DesafioNum2/src/App.tsx
@@ -1,7 +1,40 @@
+import imagePost from './img/imagePost.png';
+import likeVazio from './img/heart_empty.png';
+import comentario from './img/comments.png';
+import salvar from './img/save-instagram.png';
+import enviar from './img/send.png';
+import likeCheio from './img/heart_full.png';
+import { useState } from 'react'
+
+
 function Instagram() {
+    const [curtida, setCurtida] = useState(false)
+    const [countLike, setCountLike] = useState(Number)
     return (
-        <div>oi</div>
+        <div className="backgroundInsta">
+            <div className="Post">
+                <div className="userName">@eduaarda.a_</div>
+                <img src={imagePost} />
+                <div className="legenda">Quando o amor e a arte se encontram em mim 🤎</div>
+
+                <div className="interacoes">
+                    <button className='likevazio' onClick={() => setCurtida(!curtida)}> {/*como é uma boolean (true or false), se o state de crutida estiver falso, o ! vai inverter e tranformar em true e vice versa, usando isso, smp que clicar muda o estado */}
+                        <img src={curtida ? likeCheio : likeVazio} /> {/*curtida true ent lie cheio, curtida false, like vazio */}
+                    </button>
+                    <button className='comentario'>
+                        <img src={comentario} />
+                    </button>
+                    <button className='compartilhar'>
+                        <img src={enviar} />
+                    </button>
+                    <button className='salvar'>
+                        <img src={salvar} />
+                    </button>
+                </div>
+                <footer className="rodape"></footer>
+            </div>
+        </div>
     )
 }
 
-export default Instagram;
+export default Instagram; 
