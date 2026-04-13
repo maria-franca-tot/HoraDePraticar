83542f5fa88b8244eee2cc1565260dd5b1061337
maria-franca-tot <maria.franca.toteduca@email.com>
2026-04-13 13:17:33 -0300

    Término do desafio  n1

--- DesafioNum1/src/App.tsx
+++ DesafioNum1/src/App.tsx
@@ -1,46 +1,52 @@
 import "./App.css";
 import imagem from "./img/image.png";
-//import { useState } from "react"; 
+import { useState } from "react";
+//use state vai guardar a informacao q muda na tela OU SEJA vai vai guardar a informacao de qual alternativa que eu cliquei dando um numero de indice pra ela onde minha funcao vai  verificar se ta certo e aparecer a msg na tela
 
-//import { Button, Grid } from '@mui/material/Grid';
-//import Grid from 
-//use state vai guardar a informacao q muda na tela OU SEJA vai vai guardar a informacao
-//de qual alternativa que eu cliquei dando um numero de indice pra ela onde minha funcao vai 
-// verificar se ta certo e aparecer a msg na tela
 const alternativas = ['Tomar uma decisão imediata, desativando temporariamente o sistema e assumindo os riscos.',
-  'Convocar uma forca-tarefa para investigar a fundo e tomar uma decisão coletiva em 2 horas.',
+  'Convocar uma força-tarefa para investigar a fundo e tomar uma decisão coletiva em 2 horas.',
   'Esperar instruções da diretoria e evitar agir até ter um respaldo formal.'];
-  
-  export function Exercicio(
-    { title,
+
+const letras = ['A', 'B', 'C']
+
+export function Exercicio(
+  { title,
     text,
     subtitle,
-    italictext, }:
+    italictext,
+    feedback,
+    correctAlternative }:
     {
       text: string;
       title: string;
       subtitle: string;
       italictext: string;
       alternativas: string[];
-      alternativaCorreta: number;
-      
+      correctAlternative: number;
+
       feedback: {
-        correct: {
-          title: string;
-          text: string;
-        };
-        incorrect: {
-          title: string;
-          text: string;
-        };
+        correct: { title: string; text: string };
+        incorrect: { title: string; text: string };
       };
     }
-    
-
-  ) {
-    //const [selecionada, setSelecionada] = useState<number | null>(null); 
-    //ou ela pode ser um numero ou nao pode ser nd (null) pq qnd eu clico ela vira um numero
+) {
+  const [mostrarFeedBack, setMostrarFeedBack] = useState(false)
+  const [selecionada, setSelecionada] = useState<number | null>(null);
+  //ou ela pode ser um numero ou nao pode ser nd (null) pq qnd eu clico ela vira um numero
   //selecionada → valor atual setSelecionada → função que muda o valor
+  const [resultado, setResultado] = useState<"correct" | "incorrect" | null>(null);
+
+
+  function verificarResp() {
+    if (selecionada === null) return; //se n tiver nd clicado ele vai evitar erro
+    if (selecionada === correctAlternative) {
+      setResultado("correct")
+    } else {
+      setResultado("incorrect")
+    }
+
+    setMostrarFeedBack(true) //aq vai abrir o pop up do feedback certo ou errado
+  }
 
   return (
     <div className="cardQuestao">
@@ -58,21 +64,51 @@ const alternativas = ['Tomar uma decisão imediata, desativando temporariamente 
         <div className="OpcoesDireita">
           <div className="alternativas">
             {alternativas.map((alternativa, index) => (
-              <button className="opcao" key={index}> <div className="circulo">{letras[index]}</div> 
-                {alternativa}
+              <button className="opcao" key={index} onClick={() => setSelecionada(index)}> {/*quando clicar, guarda o número dessa alternativa*/}
+                <div className="circulo">{letras[index]}</div>
+                <span>{alternativa}</span>
               </button>
             ))}
+
           </div>
+
           <div className="BoxBotao">
-            <button className="btn"><strong>Enviar</strong></button>
+            <button className="btn" onClick={verificarResp}><strong>Enviar</strong></button>
+
+            {mostrarFeedBack && (
+              <div className="overlay">
+                <div className="feedback">
+                  {resultado === "correct" ? (
+                    <>
+                      <h3>{feedback.correct.title}</h3>
+                      <p>{feedback.correct.text}</p>
+                      <button className="btn" onClick={() => {  /*quando clica no botao limpa a selecao e a msg  */
+                        setSelecionada(null);
+                        setResultado(null);
+                        setMostrarFeedBack(false);
+                      }}>CONTINUAR</button>
+                    </>
+                  ) : (
+                    <>
+                      <h3>{feedback.incorrect.title}</h3>
+                      <p>{feedback.incorrect.text}</p>
+                      <button className="btn" onClick={() => {
+                        setSelecionada(null);
+                        setResultado(null);
+                        setMostrarFeedBack(false);
+                      }}>TENTE NOVAMENTE</button>
+                    </>
+                  )}
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
 
     </div>
+
   )
 }
 
-
-const letras = ['A', 'B', 'C']
 export default Exercicio;
--- DesafioNum1/src/main.tsx
+++ DesafioNum1/src/main.tsx
@@ -1,22 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Exercicio from './App.tsx'
 //import Grid from "@mui/material/Grid";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App
-     title={'Cenário '}
+    <Exercicio
+     title={'Cenário 1'}
       text={'Você lidera uma equipe que está enfrentando um erro crítico em um sistema de produção. O erro pode parar a operação por 24h e gerar prejuízo. A diretoria quer uma solução “pra ontem”, mas você ainda não sabe exatamente a origem do problema.'}
-      subtitle={'"Você tem três alternativas, pense bem antes de escolher:"'}
+      subtitle={'Você tem três alternativas, pense bem antes de escolher:'}
       italictext={'Selecione uma alternativa'} 
       alternativas={
         ['Tomar uma decisão imediata, desativando temporariamente o sistema e assumindo os riscos', 
-        'Convocar uma forca-tarefa para investigar a fundo e tomar uma decisão coletiva em 2 horas.', 
+        'Convocar uma força-tarefa para investigar a fundo e tomar uma decisão coletiva em 2 horas.', 
         'Esperar instruções da diretoria e evitar agir até ter um respaldo formal.']
 
-      } alternativaCorreta={1}
+      } correctAlternative={1}
 
       feedback={{
         correct: {
