b25cf0e1f267652b8b80a863e0e3f6c90c934ce3
maria-franca-tot <maria.franca.toteduca@email.com>
2026-04-14 09:59:30 -0300

    teste

--- DesafioNum2/src/App.tsx
+++ DesafioNum2/src/App.tsx
@@ -1,15 +1,33 @@
 import imagePost from './img/imagePost.png';
 import likeVazio from './img/heart_empty.png';
-import comentario from './img/comments.png';
-import salvar from './img/save-instagram.png';
-import enviar from './img/send.png';
 import likeCheio from './img/heart_full.png';
-import { useState } from 'react'
+import comentario from './img/comments.png';
+import comentarios from './img/comentarios.png';
+import salvar from './img/save-instagram.png';
+import salvo from './img/salvo.png';
+import enviar from './img/send.png';
+import { useState } from 'react';
 
 
 function Instagram() {
     const [curtida, setCurtida] = useState(false)
-    const [countLike, setCountLike] = useState(Number)
+    const [coments, setComents] = useState(false)
+    const [countLike, setCountLike] = useState(512) //botei um valor aleatorio so pra me basear
+    const [mostrarComentarios, setMostrarComentarios] = useState(false)
+
+    function VerComentarios() {
+        setMostrarComentarios(!mostrarComentarios)
+    }
+
+    function ClickLike() {
+        if (curtida) {
+            setCountLike(countLike - 1)
+        } else {
+            setCountLike(countLike + 1)
+        }
+        setCurtida(!curtida); {/*como é uma boolean (true or false), se o state de crutida estiver falso, o ! vai inverter e tranformar em true e vice versa, usando isso, smp que clicar muda o estado */ }
+    }
+
     return (
         <div className="backgroundInsta">
             <div className="Post">
@@ -18,12 +36,16 @@ function Instagram() {
                 <div className="legenda">Quando o amor e a arte se encontram em mim 🤎</div>
 
                 <div className="interacoes">
-                    <button className='likevazio' onClick={() => setCurtida(!curtida)}> {/*como é uma boolean (true or false), se o state de crutida estiver falso, o ! vai inverter e tranformar em true e vice versa, usando isso, smp que clicar muda o estado */}
+                    <button className='likevazio'
+                        onClick={ClickLike}>
                         <img src={curtida ? likeCheio : likeVazio} /> {/*curtida true ent lie cheio, curtida false, like vazio */}
+                        {countLike}
                     </button>
-                    <button className='comentario'>
-                        <img src={comentario} />
+
+                    <button className='comentario' onClick={VerComentarios}>
+                        <img src={coments ? comentarios : comentario} />
                     </button>
+                    
                     <button className='compartilhar'>
                         <img src={enviar} />
                     </button>
